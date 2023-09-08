1bef092f036d217adf207154a0f84cf999b6e732
Jun <xjunsantos@gmail.com>
2023-09-08 14:27:14 +0800

    Upload files

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home"><h1>Home</h1></div>
+  <button @click="decreaseCounter">-</button>
+  <span> {{ counter }} </span>
+  <button @click="increaseCounter">+</button>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    methods: {
+      increaseCounter() {
+        this.counter++
+      },
+      decreaseCounter() {
+        this.counter--
+      }
+    }
+  }
+</script>
