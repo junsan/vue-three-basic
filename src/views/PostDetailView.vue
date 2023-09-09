620ba1cffb46b7c1b54d077ce6bc7d0f465cc593
Jun <xjunsantos@gmail.com>
2023-09-09 22:45:25 +0800

    Routes and parameter

---
+++ src/views/PostDetailView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="post-detail">
+    <h1>Post Detail</h1>
+    <p>This post is an id of {{  $route.params.id }}</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/views/PostsView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="posts">
+    <h1>Posts</h1>
+    <ul>
+      <li><RouterLink to="/postDetail/1">Post 1</RouterLink></li>
+      <li><RouterLink to="/postDetail/2">Post 2</RouterLink></li>
+      <li><RouterLink to="/postDetail/3">Post 3</RouterLink></li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+import { vAutofocus } from '../directives/vAutofocus';
+</script>
+
+<style>
+
+</style>
