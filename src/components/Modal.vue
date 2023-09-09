de3f03faed5e16d37c0a694a336b93246c3d7699
Jun <xjunsantos@gmail.com>
2023-09-10 05:17:48 +0800

    Child components

---
+++ src/components/Modal.vue
@@ -0,0 +1,21 @@
+<template>
+    <teleport to=".modal-container">
+      <div class="modal">
+        <h4>Modal</h4>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Delectus quibusdam laboriosam, quisquam recusandae, quos amet vero voluptatum doloremque expedita aut labore repudiandae, sint rem ex voluptatibus aspernatur magnam atque vel.</p>
+        <button>Hide modal</button>  
+      </div>
+    </teleport>
+</template>
+
+<style>
+.modal {
+  background-color: bisque;
+  padding: 10px;
+  position: absolute;
+  left: 0;
+  top: 0;
+  width: 100%;
+  z-index: 1;
+}
+</style>
